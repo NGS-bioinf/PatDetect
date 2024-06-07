22c9a8488185cb0249bf77ff2ef63927000e7232
mbosilj <76106529+martin1196@users.noreply.github.com>
2024-06-07 11:21:12 +0200

    Add files via upload

---
+++ long_reads_detection/run_workflow.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# A script that controls the flow and order of long reads analysis. 
+
+# Maintaince: Martin Bosilj
+# Author: Alen Suljič
+
+# Prerequisites:
+# - Have snakemake installed and placed all singularity images
+# - Edit parameters in config.yml file. Here define number of threads (max 128) and RAM max limit (double check). 
+# - Check input path (the defined path must end above the folder containing reads). Rename folder if necessary (e.g. rename "barcode01" to "sample01").
+
+# Suggestion: befor run use "-n" flag to perform dry-run
+# Command example: bash run_workflow.sh
+#################################################################################################
+echo "Analysis start" && date
+
+snakemake --unlock
+snakemake -c128 --resources mem_mb=1000000 --rerun-incomplete -k --printshellcmds --use-singularity -n
+
+echo "Analysis complete" && date
---
+++ short_pe_reads_detection/run_workflow.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# A script that controls the flow and order of short PE reads analysis. 
+
+# Maintaince: Martin Bosilj
+# Author: Alen Suljič
+
+# Prerequisites:
+# - Have snakemake installed and placed all singularity images
+# - Edit parameters in config.yml file. Here define number of threads (max 128) and RAM max limit (double check). 
+# - Check PE reads (must end with "_R1.fastq.gz" and "_R2.fastq.gz")
+
+# Suggestion: befor run use "-n" flag to perform dry-run
+# Command example: bash run_workflow.sh
+#################################################################################################
+echo "Analysis start" && date
+
+snakemake --unlock
+snakemake -c128 --resources mem_mb=1000000 --rerun-incomplete -k --printshellcmds --use-singularity -n
+
+echo "Analysis complete" && date
